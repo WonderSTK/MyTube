07ae6a0d21f03c94fc616a41c9c4029823af808a
WonderSTK <mehulparmar9694@gmail.com>
2024-09-12 18:07:01 +0530

    environment variable setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-//const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-const GOOGLE_API_KEY = "AIzaSyAi4JAJepgPr6d2tWwPgnt2uYsm1OjQAw8";
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
 
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
